5f56600cbe7cfbac2697698136f0e9d59a49a772
laurarundle <rundlel@tcd.ie>
2017-03-17 20:33:46 +0000

    UIAlertController, can grant permission for calendar access

--- GymTime/PersonalisedTimesViewController.swift
+++ GymTime/PersonalisedTimesViewController.swift
@@ -14,11 +14,79 @@ import Firebase
 class PersonalisedTimeViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
+       checkAuthorisation()
         getEvents()
         
     }
     
+    func checkAuthorisation()
+    {
+            let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+            
+            switch (status) {
+            case EKAuthorizationStatus.notDetermined:
+                // This happens on first-run
+                print ("not Determined")
+                requestAccessToCalendar()
+            case EKAuthorizationStatus.authorized:
+                // Things are in line with being able to show the calendars in the table view
+               print("authorised")
+            case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
+                // We need to help them give us permission
+                print("restricted")
+                alertTheUser()
+            }
+    }
+
+
+
+    func requestAccessToCalendar() {
+        EKEventStore().requestAccess(to: .event, completion: {
+            (accessGranted: Bool, error: Error?) in
+        
+            if accessGranted == true {
+                DispatchQueue.main.async(execute: {
+                //    self.loadCalendars()
+                 //   self.refreshTableView()
+                    print("request Access")
+                    
+            })
+            } else {
+                DispatchQueue.main.async(execute: {
+                   // self.needPermissionView.fadeIn()
+                    self.alertTheUser()
+                })
+            }
+        })
+}
+
+
+    func alertTheUser(){
+        /*let alert = UIAlertController(title: "We Need Permission", message: "GymTime needs access to your calendar in order to tell you the best time for you", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "Go To Settings", style: UIAlertActionStyle.destructive, handler: { action in
+            
+            let openSettingsUrl = URL(string: UIApplicationOpenSettingsURLString)
+            UIApplication.shared.openURL(openSettingsUrl!)
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        
+        alert.show()*/
+        
+        let alert = UIAlertController(title: "GymTime needs permission",
+                                      message: "GymTime needs access to your calendar in order to tell you the best time for you",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Go To Settings", style: .default, handler: { action in
+            let openSettingsUrl = URL(string: UIApplicationOpenSettingsURLString)
+            UIApplication.shared.openURL(openSettingsUrl!)
+
+        }))
+       present(alert, animated: true, completion: nil)
+        
     
+    }
+
     func getEvents()
     {
         let eventStore: EKEventStore = EKEventStore()
@@ -46,6 +114,5 @@ class PersonalisedTimeViewController: UIViewController {
             }
         }
     }
-    
     
 }
